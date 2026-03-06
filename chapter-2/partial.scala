93e4c2d3a7f2d7ffbc9522c405cff7dae5eb184f
Juan Alvarez <31358977+juanzenn@users.noreply.github.com>
2026-03-05 21:38:34 -0400

    feat: create partial function

--- chapter-2/partial.scala
+++ chapter-2/partial.scala
@@ -8,7 +8,7 @@ def uncurry[A,B,C](f: A => B => C): (A,B) => C =
   (a, b) => f(a)(b)
 
 def compose[A,B,C](f:B=>C, g: A => B): A => C =
-  
+  (a) => f(g(a))
 
 @main def main() =
   def add(a: Int, b: Int): Int =
